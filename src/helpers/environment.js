48002ee73e2e8316a3bb7dd93d9d23d72bf1a362
tjreynolds91 <tjreynolds91@gmail.com>
2020-12-08 15:20:16 -0500

    new push

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -9,7 +9,7 @@ switch (window.location.hostname) {
   // this is the deployed react application
   case "trailflow.herokuapp.com": // client url
     // this is the full url of your deployed API
-    APIURL = "https://tjr-trailflow.herokuapp.com"; //server url
+    APIURL = "https://tjr-trailflow.herokuapp.com/"; //server url
 }
 
 export default APIURL;
